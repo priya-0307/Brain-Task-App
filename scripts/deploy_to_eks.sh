b99521a31a01562262d50b9738fa83e3ede319ff
Priyadharshini <priyamathiyalakan03@gmail.com>
2025-11-11 12:57:27 +0530

    Update deploy_to_eks.sh

--- scripts/deploy_to_eks.sh
+++ scripts/deploy_to_eks.sh
@@ -13,10 +13,15 @@ IMAGE_URI=${700664161347.dkr.ecr.ap-south-1.amazonaws.com/brain-tasks-app:latest
 aws eks update-kubeconfig --region ${REGION} --name ${CLUSTER_NAME}
 
 
+
+
 # Replace image placeholder in deployment and apply
-sed -i "s|<ECR_IMAGE_PLACEHOLDER>|${IMAGE_URI}|g" k8s/deployment.yaml
+sed -i "s|<ECR_IMAGE_PLACEHOLDER>|${IMAGE_URI}|g" deployment.yaml
 kubectl apply -f deployment.yaml -n ${NAMESPACE}
 kubectl apply -f service.yaml -n ${NAMESPACE}
+
+# Update kubernetes Deployment image 
+kubectl --kubeconfig "$KUBECONFIG" -n brain-tasks-app set image deployment/brain-tasks-deployment brain-tasks-container=$IMAGE_URI --record
 
 
 
